773b0849c1dc0581a37889c567c29bcaaf333791
Amaplan <ee2258203638@qq.com>
2023-08-24 22:16:01 +0800

    第一次提交

---
+++ ee.txt/cartpole.py
@@ -0,0 +1,8 @@
+import gym
+import numpy as np
+import time
+
+def figitize_state(observation):
+    p,v,a,w = observation
+    d = num_digitized
+    pn = np.digitize(p,np.linespace(-2.4,2.4,d+1)[1:-1])
